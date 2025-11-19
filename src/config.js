41c947fb49702600eacf83daec38c3e393dcc98f
neradehiu <hieuihhghkj@gmail.com>
2025-11-19 11:26:18 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ const isLocalhost = window.location.hostname === 'localhost';
 
 const BE_URL = isLocalhost
   ? 'http://localhost:3000'           // dev: backend local
-  : 'https://t-m-be.onrender.com';    // prod: backend Render (SỬA TÊN NÀY CHO ĐÚNG)
+  : 'https://t-m-dw2u.onrender.com';    // prod: backend Render (SỬA TÊN NÀY CHO ĐÚNG)
 
 export const API_BASE_URL = `${BE_URL}/api`;
 export const ASSET_BASE_URL = BE_URL;
